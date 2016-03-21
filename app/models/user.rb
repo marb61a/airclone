245fb0113c11916b000fdc82dab448f79deb16dc
marb <marb61a@gmail.com>
2016-03-21 11:53:05 +0000

    added basic user model and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates :fullname, presence: true, length: {maximum: 50}
+
+  has_many :rooms
+  has_many :reservations
+  has_many :reviews
+  
+  def self
+    
+  end
 end
