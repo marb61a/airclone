cb8958b10cd81627060f5dd19a769a6fa821df59
marb <marb61a@gmail.com>
2016-03-21 11:44:18 +0000

    added basic users and rooms controllers

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,38 @@
+class RoomsController < ApplicationController
+    before_action :set_room, only: [:show, :edit, :update]
+    before_action :authenticate_user!, except: [:show]
+    
+    def index
+        
+    end
+    
+    def show
+        
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+    private
+        
+        def set_room
+        end
+        
+        def room_params
+            
+        end
+    
+end
