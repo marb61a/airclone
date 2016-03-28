43af5325fc3e7e7eef2741afc9a1374578132126
marb <marb61a@gmail.com>
2016-03-28 13:36:45 +0000

    added photos controller and updated rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,19 +3,20 @@ class RoomsController < ApplicationController
     before_action :authenticate_user!, except: [:show]
     
     def index
-        
+        @rooms = current_user.rooms
     end
     
     def show
+        @photos = @room.photos
         
     end
     
     def new
-        
+        @room = current_user.rooms.build
     end
     
     def create
-        
+        @room = current_user.rooms.build(room_params)
     end
     
     def edit
@@ -29,10 +30,13 @@ class RoomsController < ApplicationController
     private
         
         def set_room
+            @room = Room.find(params[:id])
         end
         
         def room_params
-            
+            params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, 
+                                        :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, 
+                                        :is_heating, :is_internet, :price, :active)
         end
     
 end
