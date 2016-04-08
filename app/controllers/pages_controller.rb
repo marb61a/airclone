0f4f9a3d572a94396882605af666072ed1964fec
marb <marb61a@gmail.com>
2016-04-08 12:54:35 +0000

    updated pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,20 @@ class PagesController < ApplicationController
     end
     
     def search
-        
+        if params[:search].present? && params[:search].strip != ""
+            session[:loc_search] = params[:search]
+        end
+  	    
+  	    if session[:loc_search] && session[:loc_search] != ""
+  	        @rooms_address = Room.where(active: true).near(session[:loc_search], 5, order: 'distance')
+  	    else
+  	        @rooms_address = Room.where(active: true).all
+  	    end
+  	    
+  	    @search = @rooms_address.ransack(params[:q])
+      	@rooms = @search.result
+    
+      	@arrRooms = @rooms.to_a
+  	    
     end
 end
