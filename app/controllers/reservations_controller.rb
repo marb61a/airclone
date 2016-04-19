c5ba4fb0ec11722bd64e0b0ef18f7e455e10b942
marb <marb61a@gmail.com>
2016-04-19 13:36:05 +0000

    updated reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,11 +2,22 @@ class ReservationsController < ApplicationController
     before_action :authenticate_user!,  except: [:notify]
     
     def preload
-        
+        room = Room.find(params[:room_id])
+        today = Date.today
+        reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+
+		render json: reservations
     end
     
     def preview
+        start_date = Date.parse(params[:start_date])
+        end_date = Date.parse(params[end_date])
         
+        output ={
+            conflict: is_conflict(start_date, end_date)
+        }
+        
+        render json: output
     end
     
     def create
@@ -20,16 +31,19 @@ class ReservationsController < ApplicationController
     
     protect_from_forgery except[:your_trips]
         def your_trips
-            
+            @trips = current_user.reservations.where("status = ?", true)
         end
         
         def your_reservations
-            
+            @rooms = current_user.rooms
         end
     
     private
         def is_conflict(start_date, end_date)
+            room = Room.find(params[:room_id])
             
+            check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+            check.size > 0? true : false
         end
         
         def reservation_params
