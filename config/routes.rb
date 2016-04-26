8d47ce2b90a1422257c39e04824fa473bd45ce21
marb <marb61a@gmail.com>
2016-04-26 09:57:12 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
   root 'pages#home'
   
-  devise_for :users
+  devise_for :users,
+  :path => '',
+  :path_name => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
+  :controllers => {:omniauth_callbacks => 'omniauth_callbacks'},
+  :registrations => 'registrations'
   
   resources :users, only: [:show]
   resources :rooms
+  resources :photos
   
   resources :rooms do
     resources :reservations, only: [:create]
+  end
+  
+  resources :rooms do
+    resources :reviews, only: [:create, :destroy]
   end
   
   # The priority is based upon order of creation: first created -> highest priority.
