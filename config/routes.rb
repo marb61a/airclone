f8b15119365c3511026963bb18ff20090c19d22e
marb <marb61a@gmail.com>
2016-04-26 10:07:00 +0000

    added routes and migration

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,24 @@ Rails.application.routes.draw do
     resources :reservations, only: [:create]
   end
   
+  resources :conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
+  end
+  
   resources :rooms do
     resources :reviews, only: [:create, :destroy]
   end
+  
+  get '/preload' => 'reservations#preload'
+  get '/preview' => 'reservations#preview'
+
+  get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
+
+  post '/notify' => 'reservations#notify'
+  post '/your_trips' => 'reservations#your_trips'
+
+  get '/search' => 'pages#search'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
