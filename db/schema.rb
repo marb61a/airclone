7f8df47cedeec13cf3ebb8859be2d94512da949c
marb <marb61a@gmail.com>
2016-04-26 10:33:07 +0000

    added css and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422123601) do
+ActiveRecord::Schema.define(version: 20160426100838) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -42,6 +42,32 @@ ActiveRecord::Schema.define(version: 20160422123601) do
   end
 
   add_index "photos", ["room_id"], name: "index_photos_on_room_id"
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "room_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer  "price"
+    t.integer  "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reservations", ["room_id"], name: "index_reservations_on_room_id"
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id"
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "star",       default: 1
+    t.integer  "room_id"
+    t.integer  "user_id"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "reviews", ["room_id"], name: "index_reviews_on_room_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "rooms", force: :cascade do |t|
     t.string   "home_type"
